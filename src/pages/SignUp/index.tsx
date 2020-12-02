1314146f398f66563ca35ff9ae23772765fbac1b
Davidxy9 <davidwilliamlima@hotmail.com>
2020-12-02 13:59:04 -0300

    autenticação de user

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -6,6 +6,8 @@ import { Form } from '@unform/mobile';
 import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
 
+import api from '../../services/api';
+
 import getValidationErrors from '../../utils/getValidationErrors';
 
 
@@ -46,10 +48,14 @@ const SignUp: React.FC = () => {
         abortEarly: false,
       });
 
-      /*  await api.post('/users', data);
+      await api.post('/users', data);
 
-       history.push('/'); */
+      Alert.alert(
+        'Cadastro realizado com sucesso!',
+        'Você já pode fazer login na aplicação.'
+      );
 
+      navigation.goBack();
 
     } catch (err) {
       if (err instanceof Yup.ValidationError) {
@@ -65,7 +71,7 @@ const SignUp: React.FC = () => {
         'Ocorreu um erro ao fazer cadastro, tente novamente.',
       );
     }
-  }, []);
+  }, [navigation]);
 
 
   return (
